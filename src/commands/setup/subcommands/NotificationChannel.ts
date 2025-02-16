bb320db3c841ad3b34e03f0fda366fb9003cd32e
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2025-02-16 21:00:47 +0200

    Added webhookId option in the /setup notification-channel command

--- src/commands/setup/subcommands/NotificationChannel.ts
+++ src/commands/setup/subcommands/NotificationChannel.ts
@@ -8,35 +8,41 @@ const setupNotificationChannel  = {
     data : new SlashCommandSubcommandBuilder()
     .setName('notification-channel')
     .setDescription('Set The Channel to get build, CI and Bot notificaions in.')
-    .addChannelOption(option=>option.setName('channel').setDescription('Channel for notifications')),
+    .addChannelOption(option=>option.setName('channel').setDescription('Channel for notifications'))
+    .addStringOption(option=>option.setName('webhook-id').setDescription('The Webhook Id associated with your webhook url')),
 
     async execute(interaction: ChatInputCommandInteraction){
 
         await interaction.deferReply({flags: MessageFlags.Ephemeral})    ///Maybe reply and delete later?
         
         const channelName = interaction.options.getChannel("channel");
-
+        const webhookId   = interaction.options.getString('webhook-id');
+        
         if(!channelName){
             return interaction.editReply({content: `${channelName} is Invalid, double check the channel name!`});
         }
 
-        const mq: MQHandler = new MQHandler("SCD-BOT-SETUP");
-        await mq.initConnection();
+        MQHandler.url = process.env.SCD_RMQ_URL as string
+        await MQHandler.connect();
+        const channel : any = await MQHandler.createChannel('SCD-CH1');
 
-        mq.sendMessage(mq.getQueueDefaultName(),JSON.stringify({
-            message: "TEST from discord",
-            guildID: interaction.guildId,
-            channelId: channelName.id
-        }))
-
+        (await MQHandler.getInstance()).sendToQueue(channel,'SCD-BOT-SETUP',Buffer.from(JSON.stringify(
+            {
+                message: "TEST from discord",
+                guildID: interaction.guildId,
+                channelId: channelName.id,
+                webhookId: webhookId
+            }))
+        )
         .then(async ()=>{ 
             const channelId = channelName.id;
                 const embed = new EmbedBuilder()
                 .setTitle("Setup Action")
                 .setDescription('/setup notifaction-channel was used to set the channel for receiving Notifactions')
                 .addFields(
-                    {name: "Channel Name", value:`${channelName}`},
-                    {name: "Channel ID", value: `${channelId}`}
+                    {name: "Webhook Id", value: `${webhookId}`},
+                    {name: "Channel Name", value:`${channelName}`, inline: true},
+                    {name: "Channel ID", value: `${channelId}`, inline: true}
                 )
                 .setTimestamp()
                 //await interaction.editReply('Notification channel set successfully');
